dbed1feb716bfbb8b6b340efedbed3d4a3a47c83
Amm4n <amman.sharif07@gmail.com>
2020-03-20 18:34:24 +0800

    task 3: fixed bug

--- src/app.js
+++ src/app.js
@@ -6,25 +6,24 @@ const app = express()
 const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')))
 
 // START READ ACCOUNT DATA //
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8', (err, file) => {
-  if (err) {
-    console.log('err', err)
-  } else {
-    accDataObj = JSON.parse(accountData)
-    console.log('accDataObj', accDataObj)
-  }
-})
-
+const accountData = fs.readFileSync(path.join(__dirname, 'accounts.json'), 'utf8')
 const accounts = JSON.parse(accountData)
 // console.log('accounts', accounts)
 // console.log('accountData', accountData)
 // END READ ACCOUNT DATA //
+
+// START READ USER DATA //
+const userData = fs.readFileSync(path.json(__dirname, 'users.json'), 'utf8')
+const users = JSON.parse(userData)
+// console.log('users', users)
+// console.log('userData', userData)
+// END READ USER DATA //
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts })
@@ -42,21 +41,6 @@ app.get('/credit', (req, res) => {
   res.render('account', { account: accounts.credit })
 })
 
-// START READ USER DATA //
-const userData = fs.readFileSync('src/json/users.json', 'utf8', err => {
-  if (err) {
-    console.log('err', err)
-  } else {
-    userDataObj = JSON.parse(userData)
-    console.log('userDataObj', userDataObj)
-  }
-})
-
-const users = JSON.parse(userData)
-// console.log('users', users)
-// console.log('userData', userData)
-// END READ USER DATA //
-
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] })
 })
@@ -73,28 +57,14 @@ app.post('/transfer', (req, res) => {
   //   amount: req.body.amount
   // }
 
-  var getSavings = _.get(accounts, 'savings')
-  var getChecking = _.get(accounts, 'checking')
-  let from = req.body.from
-  let to = req.body.to
-  let amount = req.body.amount
-  from === 'savings'
-    ? (getSavings.balance = getSavings.balance - amount)
-    : (getChecking.balance = getChecking.blance - amount)
+  accounts[req.body.from].balance -= req.body.amount
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10)
 
-  to === 'savings'
-    ? (getSavings.balance = getSavings.balance + amount)
-    : (getChecking.balance = getChecking.blance + amount)
+  let accountsJSON = JSON.stringify(accounts)
 
-  const accountsJSON = JSON.stringify(accounts)
-
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
 
   res.render('transfer', { message: 'Transfer Completed' })
-
-  // FROM Balance
-  // get current balance (savings/checking) -> currentbalance - amount -> set currentbalance = new amount
-  res.redirect('/')
 })
 
 app.get('/payment', (req, res) => {
@@ -102,12 +72,10 @@ app.get('/payment', (req, res) => {
 })
 
 app.post('/payment', (req, res) => {
-  let accCreditBalance = _.get(accounts, 'credit.balance')
-  accCreditBalance = accCreditBalance - req.body.amount
-  let accCreditAvailable = _.get(accounts, 'credit.available')
-  accCreditAvailable = parseInt(req.body.amount) + parseInt(accCreditAvailable)
-  const accountsJSON = JSON.stringify(accounts)
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+  accounts.credit.balance -= req.body.amount
+  accounts.credit.available += parseInt(req.body.amount)
+  let accountsJSON = JSON.stringify(accounts)
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
 
   res.render('payment', { message: 'Payment Successful', account: accounts.credit })
 })
